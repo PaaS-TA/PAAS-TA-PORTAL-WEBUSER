2f5f34abbfffe8f68a66c00012cd21af0c9884cd
CissC2 <d3g3nyo@gmail.com>
2018-02-27 18:26:22 +0900

    Test Page Modify

--- src/main/java/org/openpaas/paasta/portal/web/user/controller/AppController.java
+++ src/main/java/org/openpaas/paasta/portal/web/user/controller/AppController.java
@@ -49,6 +49,7 @@ public class AppController extends Common {
     *
     * */
     @RequestMapping(value = "/app/tempAppView", method = RequestMethod.GET)
+    @ResponseBody
     public ModelAndView tempAppView(){
         ModelAndView mv = new ModelAndView();
         mv.setViewName("/app/testAppMain");
