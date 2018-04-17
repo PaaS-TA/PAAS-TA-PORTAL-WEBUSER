35136c4270a6ca6926e9d642f0e596c0dd9d1a4b
박철한 <parkcheolhan@bluedigm.com>
2018-04-17 19:03:03 +0900

    테스트용

--- src/app/org/org.component.ts
+++ src/app/org/org.component.ts
@@ -1,15 +1,52 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Observable} from 'rxjs/Observable';
+import {CommonService} from '../common/common.service';
+declare var $: any; declare var jQuery: any;
 
 @Component({
   selector: 'app-org',
   templateUrl: './org.component.html',
   styleUrls: ['./org.component.css']
 })
+
+
 export class OrgComponent implements OnInit {
 
-  constructor() { }
-
+  orgs : Array<Organization> = [];
+  constructor(private common: CommonService) {
+    var url = '/portalapi/v2/orgs/getorgs';
+    this.common.doGET(url, this.common.getToken()).subscribe(data => {
+      let length = data['resources']['length'];
+      console.log(data);
+      for (var i = 0; i < length; i++) {
+        let org = new Organization();
+        org.orgname = data['resources'][i]['entity']['name'];
+        this.orgs[i] = org;
+      }
+    });
+  }
   ngOnInit() {
+    $(document).ready(() => {
+      //TODO 임시로...
+      $.getScript( "../../assets/resources/js/common2.js" )
+        .done(function( script, textStatus ) {
+          //console.log( textStatus );
+        })
+        .fail(function( jqxhr, settings, exception ) {
+          console.log( exception );
+        });
+    });
   }
 
+}
+
+class Organization
+{
+  orgname : string;
+  orgid : string;
+  orgspacename : string;
+  orgdomain : string;
+
+  constructor() { }
 }
