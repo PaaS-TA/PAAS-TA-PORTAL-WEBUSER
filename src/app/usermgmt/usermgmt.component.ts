c2a199359f6c75f6f6500687f39cef9ab29b443a
SEJI <sjchoi@bluedigm.com>
2018-04-25 18:17:27 +0900

    usermgmt logInfo [validation check]

--- src/app/usermgmt/usermgmt.component.ts
+++ src/app/usermgmt/usermgmt.component.ts
@@ -6,6 +6,9 @@ import {Observable} from "rxjs/Observable";
 import {NGXLogger} from "ngx-logger";
 import {Organization} from "../model/organization";
 import {OrgService} from "../org/org.service";
+import {FormControl} from "@angular/forms";
+import {viewWrappedDebugError} from "@angular/core/src/view/errors";
+import {validate} from "codelyzer/walkerFactory/walkerFn";
 
 declare var $: any;
 declare var jQuery: any;
@@ -22,6 +25,8 @@ export class UsermgmtComponent implements OnInit {
   token: string;
   username: string;
   password: string;
+  password_new : string;
+  password_confirm : string;
 
   // Angular에서 필요에 맞게 호출
   ngOnInit() {
@@ -41,13 +46,14 @@ export class UsermgmtComponent implements OnInit {
 
   // 이 부분은 Angular가 아닌 자바스크립트에서 실행
   constructor(private httpClient: HttpClient, private common: CommonService,
-                private userMgmtService: UsermgmtService,
-                private orgService: OrgService,
-                private logger: NGXLogger) {
+              private userMgmtService: UsermgmtService,
+              private orgService: OrgService,
+              private logger: NGXLogger) {
 
     this.user = new Observable<User>();
 
     this.userInfo();
+    // this.userPassword();
 
     console.log("###Oauth Login Value### " + this.common.getUserid());
 
@@ -57,7 +63,6 @@ export class UsermgmtComponent implements OnInit {
   }
 
   userInfo() {
-    // let params = {id: this.username, password: this.password};
       this.userMgmtService.userinfo(this.common.getUserid()).subscribe(data => {
          this.user = data;
         return data;
@@ -66,7 +71,6 @@ export class UsermgmtComponent implements OnInit {
 
   userSave(){
     let params = {userName: this.user['userName'],
-      userId: this.user['userId'],
                   tellPhone: this.user['tellPhone']};
     this.userMgmtService.userSave(this.common.getUserid(), params).subscribe(data => {
       if(data == 1){
@@ -79,5 +83,34 @@ export class UsermgmtComponent implements OnInit {
     });
   }
 
+  userPassword() {
+    let params = {userId: this.user['userId'],
+                  password: this.password,
+                  password_new : this.password_new ,
+                  paasword_confrim : this.paasword_confrim};
+    this.userMgmtService.userPassword(this.common.getUserid(), params,'').subscribe(data => {
+      this.password=null;
+      this.password_new= null;
+
+      if(this.password!=null) {
+        this.password = data.getParameter('password');
+      };
+      if (this.password_new!=null){
+        this.password_new = data.getParameter('password_new');
+      };
+
+      if(this.password_new && this.password_confirm !== this.password) return{
+        validateEqual : false
+      }
+      return null
+    });
+  }
+
+  // authResetPassword(usesrname){
+  //   let params = {userName: this.user['userName'], password: this.password};
+  //   this.userMgmtService.authResetPassword(this.common.getUserid(), params,'').subscribe(data => {
+  //
+  //   });
+  // }
 
 }
