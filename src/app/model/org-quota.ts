b826c8986313a2cbae5aa786f268eddf9e546033
비디 조현구 <hgcho@bluedigm.com>
2018-04-26 14:44:14 +0900

    조직관리 관련 commit - 할당량 표시 (진행중), 할당량 클릭 이벤트 수정 중

--- src/app/model/org-quota.ts
+++ src/app/model/org-quota.ts
@@ -9,7 +9,10 @@ export class OrgQuota {
   private _metadata;
   private _entity;
 
-  constructor(metadataParam, entityParam) {
+  private pricePolicy = 'free';
+  private pricelessPolicy = 'paid';
+
+  constructor(metadataParam?, entityParam?) {
     // initialize dummy data when constructor parameter is null or undefined.
     // initialize metadata as dummy
     this.metadata = metadataParam;
@@ -42,8 +45,8 @@ export class OrgQuota {
   private set entity(extEntity) {
     if (extEntity === null || extEntity === undefined) {
       this._entity = {
-        application_instance_limit: '(dummy)',
-        application_task_limit: '(dummy)',
+        app_instance_limit: '(dummy)',
+        app_task_limit: '(dummy)',
         instance_memory_limit: '(dummy)',
         memory_limit: '(dummy)',
         name: '(dummy)',
@@ -77,11 +80,11 @@ export class OrgQuota {
   }
 
   get applicationInstanceLimit() {
-    return this.entity.application_instance_limit;
+    return this.entity.app_instance_limit;
   }
 
   get applicationTaskLimit() {
-    return this.entity.application_task_limit;
+    return this.entity.app_task_limit;
   }
 
   get instanceMemoryLimit() {
@@ -126,5 +129,25 @@ export class OrgQuota {
 
   get trialDatabaseAllowed() {
     return this.entity.trial_database_allowed;
+  }
+
+  get price(): String {
+    if (this.name.search(this.pricePolicy) >= 0) {
+      return this.pricePolicy;
+    } else if (this.name.search(this.pricelessPolicy) >= 0) {
+      return this.pricelessPolicy;
+    }
+
+    if (this.name.search('default') >= 0) {
+      return this.pricelessPolicy;
+    }
+  }
+
+  isFree(): boolean {
+    if (this.price === 'free') {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
--- src/app/org/org-inner/org-inner.component.ts
+++ src/app/org/org-inner/org-inner.component.ts
@@ -8,6 +8,8 @@ import {OrgService} from '../org.service';
 import {Component, OnInit, Input, Output, EventEmitter, AfterContentChecked} from '@angular/core';
 import {NGXLogger} from 'ngx-logger';
 
+declare var $: any;
+declare var jQuery: any;
 
 @Component({
   selector: 'app-org-inner',
@@ -73,15 +75,33 @@ export class OrgInnerComponent implements OnInit, AfterContentChecked {
   }
 
   private setSpaces(spacesParam: Array<Space>) {
-    this.org.spaces = spacesParam;
+    if (spacesParam === null || spacesParam === undefined) {
+      this.org.spaces = [];
+    } else {
+      this.org.spaces = spacesParam.sort((spaceAndy, spaceBeak) => {
+        const createdAtAndy = spaceAndy.createdAt;
+        const createdAtBeak = spaceBeak.createdAt;
+        if (createdAtAndy === createdAtBeak) {
+          return 0;
+        } else if (createdAtAndy < createdAtBeak) {
+          return -1;
+        } else {
+          return 0;
+        }
+      });
+    }
   }
 
   get quota() {
     return this.org.quota;
   }
 
-  private setQuota(quotaParam: OrgQuota) {
-    this.org.quota = quotaParam;
+  private setQuota(quotaParam?: OrgQuota) {
+    if (quotaParam === null || quotaParam === undefined) {
+      this.org.quota = new OrgQuota();
+    } else {
+      this.org.quota = quotaParam;
+    }
   }
 
   get availableQuotas() {
@@ -89,6 +109,52 @@ export class OrgInnerComponent implements OnInit, AfterContentChecked {
   }
 
   private setAvailableQuotas(availableQuotasParam: Array<OrgQuota>) {
-    this._availableQuotas = availableQuotasParam;
+    if (availableQuotasParam === null || availableQuotasParam === undefined) {
+      this._availableQuotas = [];
+    } else {
+      this._availableQuotas = availableQuotasParam.sort((quotaA, quotaB) => {
+        const nameA = quotaA.name;
+        const nameB = quotaB.name;
+        if (nameA === nameB && nameA === 'default') {
+          return 0;
+        } else if (nameA === 'default') {
+          return -1;
+        } else if (nameB === 'default') {
+          return 1;
+        }
+
+        if (nameA === nameB) {
+          return 0;
+        } else if (nameA < nameB) {
+          return -1;
+        } else {
+          return 1;
+        }
+      });
+    }
+  }
+
+  isSelected(quota: OrgQuota) {
+    return this.quota.name === quota.name;
+  }
+
+  selectQuota($event, quota: OrgQuota, logger = this.logger) {
+    // TODO
+    if (!this.isSelected(quota)) {
+      const inputElement = $event.srcElement;
+      const inputs = inputElement.parentElement.parentElement.parentElement.getElementsByTagName('input');
+      logger.info(inputs);
+      
+      $(inputElement).attr('checked', '');
+    } 
+    logger.info(event);
+  }
+
+  private isValid(param): boolean {
+    if (param === null || param === undefined) {
+      return false;
+    } else {
+      return true;
+    }
   }
 }
