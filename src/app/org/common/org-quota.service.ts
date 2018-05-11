f703e44b951737c759530786ceabb7c45ebb045c
비디 조현구 <hgcho@bluedigm.com>
2018-05-11 10:13:39 +0900

    async, await 적용 테스트 (OrgQuotaService)

--- src/app/org/common/org-quota.service.ts
+++ src/app/org/common/org-quota.service.ts
@@ -24,7 +24,7 @@ export class OrgQuotaService {
   public getOrgQuota(orgId: string): OrgQuota {
     let quota: OrgQuota = OrgQuota.empty();
     const url: string = OrgURLConstant.URLOrgQuotaInformationHead + orgId + OrgURLConstant.URLOrgQuotaInformationTail;
-    const observable = this.common.doGET(url, this.getToken());
+    const observable = this.common.doGet(url, this.getToken());
     observable.subscribe(quotaData => {
       //quota = new OrgQuota(quotaData['metadata'], quotaData['entity']);
       if (quotaData.hasOwnProperty('metadata') && quotaData.hasOwnProperty('entity') ) {
@@ -42,6 +42,7 @@ export class OrgQuotaService {
       guid: orgId,            // org guid
       quotaGuid: quota.guid,  // quota guid to change
     };
+    /*
     const observable = this.common.doPut(url, requestBody, this.getToken());
     observable.subscribe(data => {
       const isOrg: boolean =
@@ -58,12 +59,31 @@ export class OrgQuotaService {
     });
 
     return {quotaInstance: null};
+    */
+
+    const result = async() => {
+      return await this.common.doPut(url, requestBody, this.getToken()).toPromise();
+    };
+
+    return result().then(data => {
+      const isOrg: boolean =
+        data.hasOwnProperty('metadata') && data['metadata'].hasOwnProperty('guid')
+        && data.hasOwnProperty('entity') && data['entity'].hasOwnProperty('quota_definition_guid');
+      if (isOrg) {
+        const resOrgId = data['metadata']['guid'];
+        const resQuotaId = data['entity']['quota_definition_guid'];
+
+        if (resOrgId === orgId && resQuotaId === quota.guid)
+          return {quotaInstance: quota};
+      }
+      return {quotaInstance: null}
+    });
   }
 
   public getOrgAvailableQuota(): Array<OrgQuota> {
     const quotas: Array<OrgQuota> = [];
     const url: string = OrgURLConstant.URLOrgAvailableQuotasHead + OrgURLConstant.URLOrgAvailableQuotasTail;
-    const observable = this.common.doGET(url, this.getToken());
+    const observable = this.common.doGet(url, this.getToken());
     observable.subscribe(data => {
       if (data.hasOwnProperty('resources')) {
         (data['resources'] as Array<Object>).forEach(quotaData => {
