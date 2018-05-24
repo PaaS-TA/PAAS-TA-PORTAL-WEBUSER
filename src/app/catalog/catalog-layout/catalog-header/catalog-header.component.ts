7583f73e94b66291b5a68d64fbbe680ae762cc10
박철한 <parkcheolhan@bluedigm.com>
2018-05-24 21:26:28 +0900

    카탈로그 앱 개발환경 수정

--- src/app/catalog/catalog-layout/catalog-header/catalog-header.component.ts
+++ src/app/catalog/catalog-layout/catalog-header/catalog-header.component.ts
@@ -8,10 +8,23 @@ import {Router} from "@angular/router";
   styleUrls: ['./catalog-header.component.css']
 })
 export class CatalogHeaderComponent implements OnInit {
-
+  name : string;
   constructor(private catalogService: CatalogService, private router: Router) { }
 
   ngOnInit() {
+    const url = this.router['url'].split("/")[1];
+    if(url.indexOf('detail') > 0){
+      this.name = '앱 템플릿';
+    }
+    else if(url.indexOf('development') > 0){
+      this.name = '앱 개발환경';
+    }
+    else if(url.indexOf('service') > 0){
+     this.name = '서비스';
+    }
+    else{
+      this.name = '전체보기';
+    }
   }
 
   goAppMain(){
