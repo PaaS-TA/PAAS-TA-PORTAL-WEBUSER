92a5fac50dd99f1c214ff0bd33c8d8b1800bf114
박철한 <parkcheolhan@bluedigm.com>
2018-09-05 11:11:09 +0900

    인덱스페이지 수정

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+declare var $: any;
+declare var jQuery: any;
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
@@ -9,6 +11,20 @@ export class IndexComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    $(document).ready(function() {
+      $('#fullpage').fullpage({
+        scrollBar: true,
+
+        navigation: true,
+        navigationPosition: 'right',
+
+        verticalCentered: true,
+        css3:false,
+      });
+    });
+
+
   }
 
 }
