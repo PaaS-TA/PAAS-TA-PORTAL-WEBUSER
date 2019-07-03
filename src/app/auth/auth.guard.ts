1f1386c918a008c046a03c55702ecfc150747dc2
swmoon <swmoon@bluedigm.com>
2019-07-03 10:33:24 +0900

    api_uri , uaa_uri, Seq가 업을 경우 인덱스 페이지로 이동되도록 수정

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
-import {CommonService} from "../common/common.service";
+import {CommonService} from '../common/common.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
@@ -12,33 +12,40 @@ export class AuthGuard implements CanActivate {
   canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
 
 
-
-    if (this.common.getSessionTime() == null || JSON.parse(this.common.getSessionTime()) < new Date().getTime()) {
-      this.common.signOut();
-    } else {
-      this.common.refreshSession();
-    }
-
     if (this.common.getSeq() == null || this.common.getSeq() === '') {
       this.router.navigate(['/']);
     } else {
       return true;
     }
 
-    if (this.common.getToken() != null) {
+    if (this.common.getUaaUri() != null) {
       return true;
     } else {
-      this.common.signOut();
+      this.router.navigate(['/']);
     }
 
     if (this.common.getApiUri() != null) {
       return true;
     } else {
-      this.common.signOut();
+      this.router.navigate(['/']);
     }
 
+    if (this.common.getToken() != null) {
+      return true;
+    } else {
+      this.router.navigate(['/']);
+      // this.common.signOut();
 
-    this.router.navigate(['/login'], {queryParams: {returnUrl: state.url}});
+    }
+
+    if (this.common.getSessionTime() == null || JSON.parse(this.common.getSessionTime()) < new Date().getTime()) {
+      this.router.navigate(['/']);
+      // this.common.signOut();
+    } else {
+      this.common.refreshSession();
+    }
+
+    // this.router.navigate(['/login'], {queryParams: {returnUrl: state.url}});
     return false;
   }
 }
