36663c1fa61127d2a5f3ee26b8ac390bd5b62685
sjchoi <sjchoi@bluedigm.com>
2019-07-09 11:12:32 +0900

    log(params) 삭제

--- src/app/org/org-temp/org-temp.component.ts
+++ src/app/org/org-temp/org-temp.component.ts
@@ -439,7 +439,6 @@ export class OrgTempComponent implements OnInit, AfterViewChecked {
 
     /*userInviteEmailSend*/
     this.common.getInfra(this.common.getSeq()).subscribe(infra => {
-      this.log.debug(params);
       this.common.doPostMulti(infra['apiUri']+'/commonapi/v2/email/inviteOrg', infra['authorization'], params, '').subscribe(data => {
         if (data) {
           this.common.alertMessage(this.translateEntities.alertLayer.sendEmailSuccess, true);
