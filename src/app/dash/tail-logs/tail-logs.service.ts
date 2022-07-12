b880ddd8590e905320ea181df8208b300cccec0c
okpc579 <ski579579@naver.com>
2022-07-12 07:14:11 +0000

    don't use websocket

--- src/app/dash/tail-logs/tail-logs.service.ts
+++ src/app/dash/tail-logs/tail-logs.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/map';
 
-import {WebsocketService} from './websocket.service';
 import { Observable, Subject } from 'rxjs/Rx';
 
 import {CommonService} from '../../common/common.service';
